12015f7951f8ef05aa71526221d987bfcf5f4587
muanpee <muanpee_t@cmu.ac.th>
2025-10-25 15:50:03 +0700

    add security

---
+++ backend/src/main/java/se331/rest/security/token/TokenService.java
@@ -0,0 +1,5 @@
+package se331.rest.security.token;
+
+public interface TokenService {
+    void save(Token token);
+}
---
+++ backend/src/main/java/se331/rest/security/token/TokenType.java
@@ -0,0 +1,5 @@
+package se331.rest.security.token;
+
+public enum TokenType {
+  BEARER
+}
